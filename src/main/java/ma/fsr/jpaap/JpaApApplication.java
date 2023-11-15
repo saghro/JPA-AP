f99b7a1bb880238f97cdc95f36dafdd623b0a5f8
saghro <ayoubsaghro27@gmail.com>
2023-11-15 11:49:36 +0100

    first commit

---
+++ src/main/java/ma/fsr/jpaap/JpaApApplication.java
@@ -0,0 +1,13 @@
+package ma.fsr.jpaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaApApplication.class, args);
+  }
+
+}
