7b3778563cfa978809833954b75821a645df5159
saghro <ayoubsaghro27@gmail.com>
2023-11-16 12:57:08 +0100

    ct jpa

--- src/main/java/ma/fsr/jpaap/JpaApApplication.java
+++ src/main/java/ma/fsr/jpaap/JpaApApplication.java
@@ -1,13 +1,18 @@
 package ma.fsr.jpaap;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaApApplication {
+public class JpaApApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(JpaApApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+
+  }
 }
